0d1e9175ccbf0a0b001b493003650e9c3a0e7277
nikitabushuev <mrbushik1@gmail.com>
2025-02-13 22:14:38 +0400

    add frontend url

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(
   }),
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://localhost:4200");
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || '');
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
